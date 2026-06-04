61ff9badcebe32fe07c196b1d7c8039ea8b0ad45
emiliasuoranta <emilia.suoranta@helsinki.fi>
2026-06-04 20:11:12 +0300

    Add files via upload

---
+++ Names_3_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_Names_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "14.3px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("nimet") !== null) {
+        labelText = String(feature.get("nimet"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            displacement: [0, 0],  fill: new ol.style.Fill({color: 'rgba(213,180,60,0.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
